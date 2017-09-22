022e9b6dcb306c3c09a9e416db563c97ad0b53fe
Unknown <lucasl.pro13@gmail.com>
2017-09-22 17:09:43 +0200

    Debut partie 2 done

--- CRYPTO/TP1/HMAC/Resumes_en_C/cert.c
+++ CRYPTO/TP1/HMAC/Resumes_en_C/cert.c
@@ -8,6 +8,9 @@
 
 
 int detecter_corps(FILE *fichier){
+
+  fseek(fichier, 0, SEEK_SET);
+
   int position = 0; 
   char *line; 
   size_t len = 0;
@@ -45,7 +48,7 @@ int extraire_corps(FILE *fichier, char **corps){
   strcpy(*corps, buffer);
   free(buffer);
 
-  return nb_octets_lus-2;
+  return nb_octets_lus;
 }
 
 void calculer_MD5(unsigned char *resume, char *text, int nb_octets_lus){
@@ -56,16 +59,43 @@ void calculer_MD5(unsigned char *resume, char *text, int nb_octets_lus){
   MD5_Final (resume, &contexte);
 }
 
+void copy_debut_file(FILE *fichierToCopy, FILE* fichierCopy, char *copyName, int positionEntete){
+  unsigned char buffer[1024];
+  
+  fichierCopy = fopen(copyName, "w");
+  if(fichierCopy == NULL){printf("Erreur création fichier %s\n", copyName); return;}
+
+  fseek(fichierToCopy, 0, SEEK_SET);
+  int nb_octets_lus;
+  nb_octets_lus = fread (buffer, 1, positionEntete-2, fichierToCopy);
+  fwrite(buffer, 1, positionEntete-2, fichierCopy);
+
+  fclose(fichierCopy);
+}
+
+void ajouter_champ(FILE *fichierCopy, char *copyName, char *secret, int positionEntete){
+  int i;
+  fichierCopy = fopen(copyName, "a+");
+  printf("Position de l'entete : %d\n", positionEntete);
+  char *champText = "X-UdC_authentique: ";
+  fseek(fichierCopy, positionEntete, SEEK_SET);
+  fwrite(champText, 1, strlen(champText), fichierCopy);
+  fwrite(secret, 1, strlen(secret), fichierCopy);
+}
 
 int cert(){
     int i;
     unsigned char resume_md5[MD5_DIGEST_LENGTH];
     int nb_octets_lus;
     char *corps;
+    char *secret = "c5dcb78732e1f3966647655229729843";
 
-    // On ouvre le fichier
-    char *nom_du_fichier="../email1.txt";
-    FILE *fichier = fopen (nom_du_fichier, "r");
+    FILE *fichier, *fichierCopy;
+
+    char *nom_du_fichier="../email1.txt";    
+    char *nom_du_fichier_copy = "../email1-secure.txt";
+
+    fichier = fopen (nom_du_fichier, "r");
     if (fichier == NULL) {
       printf ("Le fichier %s ne peut pas être ouvert.\n", nom_du_fichier);
       return 0;
@@ -79,6 +109,10 @@ int cert(){
     printf("Le résumé MD5 du corps du fichier email1.txt vaut: 0x");
     for(i = 0; i < MD5_DIGEST_LENGTH; i++) printf("%02x", resume_md5[i]);
     printf("\n"); 
+
+    copy_debut_file(fichier, fichierCopy, nom_du_fichier_copy, detecter_corps(fichier));
+
+    ajouter_champ(fichierCopy, nom_du_fichier_copy, secret, detecter_corps(fichier));
     
     return 0;
 }
