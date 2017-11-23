e35bfe3739edf608db19466ac9144df4d65dbd31
Unknown <lucasl.pro13@gmail.com>
2017-11-24 00:10:44 +0100

    Debut ClientTCPv1

--- ProjetReseaux/src/TCPv1/Client.java
+++ ProjetReseaux/src/TCPv1/Client.java
@@ -1,7 +1,36 @@
 package TCPv1;
 
+import java.net.Socket;
+
 /**
  * Created by work on 23/11/17.
  */
-public class Client {
+
+public class Client extends ReseauxToolbox{
+
+    private boolean connecte;
+    private Socket sock;
+
+    private void se_connecter(){
+
+    }
+
+    private void jouer(){
+
+    }
+    @Override
+    void stopSocket(Socket soc) {
+        connecte = false;
+        if( !soc.equals(sock)) return;
+        try{
+            sock.close();
+        }catch (Exception e){e.printStackTrace();}
+    }
+
+    Client(){
+        se_connecter();
+        while(connecte)
+            jouer();
+    }
+
 }
